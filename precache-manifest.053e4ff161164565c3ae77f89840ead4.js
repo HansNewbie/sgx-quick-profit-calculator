68145ce33eac60597c980189d69b39cdd79af7e0
Hans Adrian <nick.hansadrian@gmail.com>
2018-12-02 19:04:16 +0800

    Updates

---
+++ precache-manifest.053e4ff161164565c3ae77f89840ead4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d7f7fe0e70f7bd596a4",
+    "url": "/sgx-quick-profit-calculator/static/js/runtime~main.0d7f7fe0.js"
+  },
+  {
+    "revision": "a9f7f0018a9216ba209d",
+    "url": "/sgx-quick-profit-calculator/static/js/main.a9f7f001.chunk.js"
+  },
+  {
+    "revision": "7bbd8418e4338d6a43eb",
+    "url": "/sgx-quick-profit-calculator/static/js/1.7bbd8418.chunk.js"
+  },
+  {
+    "revision": "a9f7f0018a9216ba209d",
+    "url": "/sgx-quick-profit-calculator/static/css/main.730a8130.chunk.css"
+  },
+  {
+    "revision": "8589ee5b7e70059f9d9a4768dc36d1b3",
+    "url": "/sgx-quick-profit-calculator/index.html"
+  }
+];
